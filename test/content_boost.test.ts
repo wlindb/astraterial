7617fe7dcca0bcb19966ef45ec617198118fcf80
William Lindblom <karlwilliamlindblom@gmail.com>
2025-03-22 08:16:26 +0100

    Initial commit

---
+++ test/content_boost.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ContentBoost from '../lib/content_boost-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/content_boost-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ContentBoost.ContentBoostStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
